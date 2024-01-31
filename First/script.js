23a011bdf310f82d17de4e125caa41ccfb3a1ae5
PIYUSH-IIITL <piyushnayak90020@gmail.com>
2024-01-31 23:07:42 +0530

    first animated website

---
+++ First/script.js
@@ -0,0 +1,24 @@
+
+gsap.to(".img-container",{
+    ease: Expo.easeInOut,
+    width: "100%",
+    duration :2,
+    stagger : 2
+
+})
+gsap.to(".text h1",{
+    ease: Expo.easeInOut,
+     top: 0,
+    duration :2,
+    stagger : 2
+
+})
+
+gsap.to(".text h1",{
+    ease: Expo.easeInOut,
+    delay : 2,
+     top: "-100%",
+    duration :2,
+    stagger : 2
+
+})
